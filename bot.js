11ce2bd0199e6c06bf49f5b7b5fb0733fd8b0e00
noyrirkfnsgjgo949rnjgj4ijrngk4twakjngh5 <43873315+noyrirkfnsgjgo949rnjgj4ijrngk4twakjngh5@users.noreply.github.com>
2018-10-05 09:40:27 +0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,27 @@
+const Discord = require('discord.js');
+const A7MD = new Discord.Client();
+let timer;
+console.log("BOT ONLINE");
+
+A7MD.on("guildMemberAdd", member => {
+    timer = Math.floor(Math.random() * (10000 - 5000 + 1)) + 5000
+  let words = [` 
+  **
+  :hearts:️:paperclips:.
+  :heart_eyes: :heart: 
+  لسنا الافضل لكن بوجودك نستمر , حياك ف سيرفرنا ي جمميل ؟  :musical_score::sparkles:
+                                 [https://discord.gg/NCx8rvu ] 
+  وههذي لك ي جمميل ؟ ... [ ${member}  ]
+  **`,`سلام عليكم ياحلو ,تعال ونورنا ناقصنا تواجدك:candle::black_heart:.
+  رابط:https://discord.gg/NCx8rvu -رُبما صدفة من غير حُسبان 
+  نصبح بها احبباب ؟ :heartbeat:.
+  
+  LINK : https://discord.gg/NCx8rvu `]
+  setTimeout(() =>{
+  member.createDM().then(function (channel) {
+  return channel.send(`${words[Math.floor(Math.random() * words.length)]}`) 
+}).catch(console.error)
+}, timer)
+})
+
+client.login(process.env.BOT_TOKEN);
